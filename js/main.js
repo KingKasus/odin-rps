51e7417d2793ab431c3f9408a6c6429a0def1f68
Marc Ullmann <raucus@web.de>
2022-11-25 16:32:21 +0100

    Done until Exercise end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let playerSelection;
+let computerSelection;
+let wins = 0;
+let losses = 0;
+
+
 function getComputerChoice () {
     let randNum = Math.floor(Math.random() * 3);
     switch (randNum) {
@@ -19,6 +25,70 @@ function getComputerChoice () {
     }
 }
 
-console.log(
-    getComputerChoice()
-)
+function playRound (playerSel, computerSel) {
+    playerSel = playerSelection.toLowerCase();
+    computerSel = computerSelection.toLowerCase();
+    let result = playerSel.charAt(0).toUpperCase() + playerSel.slice(1) + " vs " + computerSelection + " --> ";
+
+    if (playerSel == "rock" | playerSel == "paper" | playerSel == "scissors"
+        && computerSel == "rock" | computerSel == "paper" | computerSel == "scissors") {
+
+        if (playerSel == computerSel) {
+            return result + "Tie";;
+        }
+        else {
+            if (playerSel == "rock") {
+                if (computerSel == "paper") {
+                    losses++;
+                    return result + "You lose"
+                }
+                else {
+                    wins++;
+                    return result + "You win"
+                }
+
+            }
+            else if (playerSel == "paper") {
+                if (computerSel == "scissors") {
+                    losses++;
+                    return result + "You lose"
+                }
+                else {
+                    wins++;
+                    return result + "You win"
+                }
+
+            }
+            else {
+                if (computerSel == "rock") {
+                    losses++;
+                    return result + "You lose"
+                }
+                else {
+                    wins++;
+                    return result + "You win"
+                }
+            }
+        }
+    }
+    else {
+        console.log("Invalid selection occured")
+    }
+}
+
+function game () {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Type Rock, Paper or Scissors to make your choice. Good luck.");
+        computerSelection = getComputerChoice();
+        console.log(
+            playRound(playerSelection, computerSelection)
+        );
+        console.log(
+            "Wins: " + wins + "   Losses: " + losses 
+        );
+
+    }
+}
+
+
+game()
