69c59d7adf89f41a2041b6bb39db6f76ade26c81
Marc Ullmann <raucus@web.de>
2022-11-25 14:03:17 +0100

    First creation, simple computer choice

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getComputerChoice () {
+    let randNum = Math.floor(Math.random() * 3);
+    switch (randNum) {
+
+        case 0:
+            return "Rock";
+            break;
+        
+        case 1:
+            return "Paper";
+            break;
+        
+        case 2:
+            return "Scissors";
+            break;
+
+        default:
+            return "An error occured"
+    }
+}
+
+console.log(
+    getComputerChoice()
+)
